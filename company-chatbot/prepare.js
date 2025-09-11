d4841ceed0a51224d799de74489091a85bfbc735
mustafazuberi <mustafazuberi986@gmail.com>
2025-09-11 16:57:46 +0500

    created chunks of document

--- company-chatbot/prepare.js
+++ company-chatbot/prepare.js
@@ -1,7 +1,15 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 export async function indexTheDocument(filePath) {
     const loader = new PDFLoader(filePath, { splitPages: false });
-    const doc = (await loader.load()[0].pageContent);
-    console.log(doc)
+    const doc = (await loader.load())[0].pageContent;
+
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 500,
+        chunkOverlap: 100,
+    });
+
+    const texts = await textSplitter.splitText(doc);
+    console.log('length of texts', texts.length);
 }
