64cf300b6f1b5242582074d482a997f474c4694e
mustafazuberi <mustafazuberi986@gmail.com>
2025-09-11 18:07:18 +0500

    added chunks embeddings in pinecone db

--- company-chatbot/prepare.js
+++ company-chatbot/prepare.js
@@ -1,15 +1,33 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+import { PineconeStore } from "@langchain/pinecone";
+
+const embeddings = new OpenAIEmbeddings({
+    model: "text-embedding-3-small"
+});
+
+const pinecone = new PineconeClient({ apiKey: process.env.PINECONE_API_KEY });
+
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex,
+    maxConcurrency: 5,
+});
 
 export async function indexTheDocument(filePath) {
     const loader = new PDFLoader(filePath, { splitPages: false });
-    const doc = (await loader.load())[0].pageContent;
+    const doc = await loader.load()
 
     const textSplitter = new RecursiveCharacterTextSplitter({
         chunkSize: 500,
         chunkOverlap: 100,
     });
 
-    const texts = await textSplitter.splitText(doc);
-    console.log('length of texts', texts.length);
+    const texts = await textSplitter.splitText(doc[0].pageContent);
+    const documents = texts.map((chunk) => ({ pageContent: chunk, metadata: doc[0].metadata }));
+
+    await vectorStore.addDocuments(documents);
 }
